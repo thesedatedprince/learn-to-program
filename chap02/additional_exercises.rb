5a32958e3c0e3d1a9d44714af88045df11223ea6
Matthew <thesedatedprince@gmail.com>
2016-12-02 22:05:13 +0000

    added additional exercises

---
+++ chap02/additional_exercises.rb
@@ -0,0 +1,10 @@
+#Works out hours in a year
+puts 24 * 365
+#Works out minutes in a decade
+puts ((60 * 24) * 365) * 10
+#Works out my age in seconds
+puts (((60 * 60) * 24) * 365) * 28
+
+#Works out Author's age
+
+puts (((1160000000 / 60) /60) /24) /365
