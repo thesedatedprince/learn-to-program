bf163f6c20ab496e09c0533e64605bc5d0e4688b
Matthew <thesedatedprince@gmail.com>
2016-12-02 22:26:59 +0000

    Added chapter four exercise

---
+++ chap04/ex1.rb
@@ -0,0 +1,39 @@
+#Initial exercise
+
+puts '...you can say that again...'
+puts '...you can say that again...'
+
+#Utilisising string for efficienct
+my_string = '...you can say that again...'
+puts my_string
+puts my_string
+
+#String for name
+name = 'Anya Christina Emmanuella Jenkins Harris'
+puts 'My name is ' + name + '.'
+puts 'Wow! ' + name
+puts 'is a really long name!'
+
+#Composer string, and example of how a variable is overwritten
+composer = 'Mozart'
+puts composer + ' was "da bomb" in his day.'
+
+composer = 'Beethoven'
+puts 'But I prefer ' + composer + ', personally.'
+
+#Examples of different data types in variable
+my_own_var = 'just another ' + 'string'
+puts my_own_var
+
+my_own_var = 5 * (1+2)
+puts my_own_var
+
+#example of variable reassignment flow
+var1 = 8
+var2 = var1
+puts var1
+puts var2
+puts ''
+var1 = 'eight'
+puts var1
+puts var2
