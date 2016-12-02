9d45369f012673136a5cb45dc286d564bf7081c7
Matthew <thesedatedprince@gmail.com>
2016-12-02 22:47:56 +0000

    Add chapter 5 exercises

---
+++ chap05/ex4_AFewThingstoTryFullName.rb
@@ -0,0 +1,7 @@
+puts "Hello. What's your first name?"
+first_name = gets.chomp
+puts "Thank you, so your first name is #{first_name}. What's your middle name?"
+middle_name = gets.chomp
+puts "What an incredible middle name #{middle_name} is! Finally, what's your last name?"
+last_name = gets.chomp
+puts "So your full name is #{first_name} #{middle_name} #{last_name}. It's a pleasure to meet you!"
---
+++ chap05/ex5_AFewThingstoTryFavouriteNumber.rb
@@ -0,0 +1,3 @@
+puts "What is your favourite number?"
+fave_num = gets.chomp
+puts "So your favourite number is #{fave_num}. May I suggest a new favourite number, #{fave_num.to_i + 1}, might be more to your sophisticated tastes?"
