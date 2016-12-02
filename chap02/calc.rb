fc0ac3663a70d1c937f30a5f1c3d3befd1442f56
Matthew <thesedatedprince@gmail.com>
2016-12-02 22:01:00 +0000

    Added chap02 folder

---
+++ chap02/calc.rb
@@ -0,0 +1,22 @@
+# Intro Code
+
+puts 1+2
+
+# 2.4 Simple Arithmetic
+
+puts 1.0 + 2.0
+puts 2.0 * 3.0
+puts 5.0 - 8.0
+puts 9.0 / 2.0
+
+# 2.4 as integers
+
+puts 1+2
+puts 2*3
+puts 5-8
+puts 9/2
+
+# more complicated expressions
+
+puts 5 * (12-8) + -15
+puts 98 + (59872 /(13*8)) * -51 
